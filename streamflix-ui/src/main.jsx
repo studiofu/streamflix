5f97e15228e6dcef031a78e2a5cf6cf60b3bef26
Johnson Fu <johnson.fu@gmail.com>
2026-04-18 01:30:02 +0800

    Refactor Apollo Client setup in StreamFlix UI to use new HttpLink and SetContextLink for improved token management in GraphQL requests.

--- streamflix-ui/src/main.jsx
+++ streamflix-ui/src/main.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
 import { ApolloProvider } from '@apollo/client/react';
-import { setContext } from '@apollo/client/link/context';
+import { SetContextLink } from '@apollo/client/link/context';
 
-const httpLink = createHttpLink({
+const httpLink = new HttpLink({
   uri: 'http://localhost:4000/',
 });
 
 // This intercepts every GraphQL request and attaches the token if it exists
-const authLink = setContext((_, { headers }) => {
+const authLink = new SetContextLink((prevContext) => {
   const token = localStorage.getItem('token');
   return {
     headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : "",
-    }
-  }
+      ...prevContext.headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
 });
 
 const client = new ApolloClient({
